dbf431f33a4e1902c60b2bc19aa2edd230d1410c
kryukovaeks <47472303+kryukovaeks@users.noreply.github.com>
2023-07-18 10:45:18 +0000

    o

--- trading_bot_advisor.py
+++ trading_bot_advisor.py
@@ -27,109 +27,105 @@ date_input = st.date_input('News date:')
 
 if cryptos_input:
     cryptos = [crypto.strip() for crypto in cryptos_input.split(',')]
-    # Get historical data and plot it
-    crypto_data = ''
 
-# ...
+    fig = go.Figure()
 
-fig = go.Figure()
+    for crypto in cryptos:
+        try:
+            data = cg.get_coin_market_chart_by_id(crypto, vs_currency='usd', days=days_input)
+            prices = data['prices']
+            prices_only = [price[1] for price in prices]
+            time_stamps = [price[0] for price in prices]
+            dates = [dt.datetime.fromtimestamp(time_stamp/1000) for time_stamp in time_stamps]
+            high = max(prices_only)
+            low = min(prices_only)
+            avg = np.mean(prices_only)
+            crypto_data += f" {crypto} data for the past {days_input} days: High={high}, Low={low}, Average={avg}\n"
+            st.write(f"{crypto} data for the past {days_input} days: High={high}, Low={low}, Average={avg}")
 
-for crypto in cryptos:
-    try:
-        data = cg.get_coin_market_chart_by_id(crypto, vs_currency='usd', days=days_input)
-        prices = data['prices']
-        prices_only = [price[1] for price in prices]
-        time_stamps = [price[0] for price in prices]
-        dates = [dt.datetime.fromtimestamp(time_stamp/1000) for time_stamp in time_stamps]
-        high = max(prices_only)
-        low = min(prices_only)
-        avg = np.mean(prices_only)
-        crypto_data += f" {crypto} data for the past {days_input} days: High={high}, Low={low}, Average={avg}\n"
-        st.write(f"{crypto} data for the past {days_input} days: High={high}, Low={low}, Average={avg}")
+            # Add a trace for this cryptocurrency
+            fig.add_trace(go.Scatter(x=dates, y=prices_only, mode='lines', name=crypto))
 
-        # Add a trace for this cryptocurrency
-        fig.add_trace(go.Scatter(x=dates, y=prices_only, mode='lines', name=crypto))
+        except Exception as e:
+            st.error(f"An error occurred when fetching data for {crypto}: {str(e)}")
 
-    except Exception as e:
-        st.error(f"An error occurred when fetching data for {crypto}: {str(e)}")
-
-    # Show the figure with the graph
-    st.plotly_chart(fig)
+        # Show the figure with the graph
+        st.plotly_chart(fig)
 
 
 
-from dateutil.parser import parse
+    from dateutil.parser import parse
 
-news_dict = {}
+    news_dict = {}
 
-for term in cryptos:
-    googlenews.search(term)
-    googlenews.get_page(1)
-    news_dict[term] = googlenews.results()
-    googlenews.clear()
+    for term in cryptos:
+        googlenews.search(term)
+        googlenews.get_page(1)
+        news_dict[term] = googlenews.results()
+        googlenews.clear()
 
-# Print the news for each term
-for term, news_list in news_dict.items():
-    st.markdown(f"News for {term}:")
-    for news in news_list:
-        st.markdown(news)
-#st.markdown(news_output)
+    # Print the news for each term
+    for term, news_list in news_dict.items():
+        st.markdown(f"News for {term}:")
+        for news in news_list:
+            st.markdown(news)
+    #st.markdown(news_output)
 
 
-'''
-    # AI prompt
-    base_prompt = f"""
-    You are in control of my crypto trading profile. You should take into consideration the factors you have to determine the best trade. Here is the info:
+    '''
+        # AI prompt
+        base_prompt = f"""
+        You are in control of my crypto trading profile. You should take into consideration the factors you have to determine the best trade. Here is the info:
 
-    You can execute these commands:
+        You can execute these commands:
 
-    1. buy_crypto_price(symbol, amount)
-    2. buy_crypto_limit(symbol, amount, limit)
-    3. sell_crypto_price(symbol, amount)
-    4. sell_crypto_limit(symbol, amount, limit)
-    5. do_nothing()
+        1. buy_crypto_price(symbol, amount)
+        2. buy_crypto_limit(symbol, amount, limit)
+        3. sell_crypto_price(symbol, amount)
+        4. sell_crypto_limit(symbol, amount, limit)
+        5. do_nothing()
 
-    Use this when you don't see any necessary changes.
+        Use this when you don't see any necessary changes.
 
-    You also have access to this data:
+        You also have access to this data:
 
-    1. Historical data
-    2. News Headlines
+        1. Historical data
+        2. News Headlines
 
-    The current date and time is {dt.datetime.today()}
+        The current date and time is {dt.datetime.today()}
 
-    You are called once every 30 minutes, keep this in mind.
+        You are called once every 30 minutes, keep this in mind.
 
-    The only cryptos you can trade are {', '.join(cryptos)}.
+        The only cryptos you can trade are {', '.join(cryptos)}.
 
-    Here are the data sources:
+        Here are the data sources:
 
-    """
-
-    info_str = f"Historical data: {crypto_data}\n News: {news_output}"
-    prompt = base_prompt + "\n\n" + info_str
-    user_prompt = """
-    What should we do to make the most amount of profit based on the info? Here are your options for a response.
-
-    1. buy_crypto_price(symbol, amount) This will buy the specified amount of the specified cryptocurrency.
-    2. buy_crypto_limit(symbol, amount, limit) This will set a limit order to buy the specified amount of the specified cryptocurrency if it reaches the specified limit.
-    3. sell_crypto_price(symbol, amount) This will sell the specified amount of the specified cryptocurrency.
-    4. sell_crypto_limit(symbol, amount, limit) This will set a limit order to sell the specified amount of the specified cryptocurrency if it reaches the specified limit.
-    5. do_nothing() Use this when you don't see any necessary changes.
-
-    Choose one and explain
         """
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": prompt},
-            {"role": "user", "content": user_prompt}
-        ],
-        temperature = 0.2,
-    )
+        info_str = f"Historical data: {crypto_data}\n News: {news_output}"
+        prompt = base_prompt + "\n\n" + info_str
+        user_prompt = """
+        What should we do to make the most amount of profit based on the info? Here are your options for a response.
 
-    res = response.choices[0].message["content"]
-    res = res.replace("\\", "")
-    st.write(textwrap.fill(str(res), width=50))
-'''
+        1. buy_crypto_price(symbol, amount) This will buy the specified amount of the specified cryptocurrency.
+        2. buy_crypto_limit(symbol, amount, limit) This will set a limit order to buy the specified amount of the specified cryptocurrency if it reaches the specified limit.
+        3. sell_crypto_price(symbol, amount) This will sell the specified amount of the specified cryptocurrency.
+        4. sell_crypto_limit(symbol, amount, limit) This will set a limit order to sell the specified amount of the specified cryptocurrency if it reaches the specified limit.
+        5. do_nothing() Use this when you don't see any necessary changes.
+
+        Choose one and explain
+            """
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": user_prompt}
+            ],
+            temperature = 0.2,
+        )
+
+        res = response.choices[0].message["content"]
+        res = res.replace("\\", "")
+        st.write(textwrap.fill(str(res), width=50))
+    '''
