6379d7d9017f2b5b73f665de42c4fe3317eb06c4
kryukovaeks <47472303+kryukovaeks@users.noreply.github.com>
2023-07-19 10:16:38 +0200

    drop_duplicates

--- trading_bot_advisor.py
+++ trading_bot_advisor.py
@@ -94,7 +94,7 @@ if cryptos_input:
     #date_input = pd.to_datetime(date_input)  # Replace with the actual date input
 
     # Sort the dataframe by the 'datetime' column
-    df = news_data.sort_values(by =['crypto','datetime'], ascending=False)
+    df = news_data.sort_values(by =['crypto','datetime'], ascending=False).drop_duplicates()
 
 
 
