2bf5b0d1a8790db0cd8aefb98949f5918dc7481a
kryukovaeks <47472303+kryukovaeks@users.noreply.github.com>
2023-07-19 11:17:53 +0200

    period

--- trading_bot_advisor.py
+++ trading_bot_advisor.py
@@ -75,7 +75,7 @@ if cryptos_input:
 
     news_dict = {}
     try:
-        googlenews = GoogleNews(period='7d')
+        googlenews = GoogleNews()
         for term in cryptos:
             googlenews.search(term)
             googlenews.get_page(1)
